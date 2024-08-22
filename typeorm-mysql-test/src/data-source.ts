b6adca7b1a14b218756385ba6073c270e505c594
mrx <1320504268@qq.com>
2024-08-22 22:21:49 +0800

    note: typeorm

--- typeorm-mysql-test/src/data-source.ts
+++ typeorm-mysql-test/src/data-source.ts
@@ -4,14 +4,19 @@ import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "120.79.98.192",
     port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
+    username: "root",
+    password: "123456",
+    database: "practice",
+    synchronize: true, // 根据实体自动创建表，若表已存在则不会创建
+    logging: true,
+    entities: [User], // 指定和数据库表对应的实体，也可以通过路径来指定
     migrations: [],
-    subscribers: [],
+    subscribers: [], // 存放一些生命周期钩子函数
+    connectorPackage: 'mysql2', // 驱动包
+    poolSize: 10, // 连接池中最大连接数
+    extra: {
+        authPlugin: 'sha256_password'
+    }
 })
